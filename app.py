72035d725524c094025df8e551e50dcfbccfbe15
Scott Gall <scottarthurgall@gmail.com>
2021-04-20 13:15:20 -0500

    finished flask api

--- app.py
+++ app.py
@@ -27,7 +27,7 @@ def welcome():
         f"/api/v1.0/precipitation<br/>"
         f"/api/v1.0/stations</br>"
         f"/api/v1.0/tobs</br>"
-        f"/api/v1.0/&lt;start&gt; and /api/v1.0/&lt;start&gt;/&lt;end&gt;"
+        f"/api/v1.0/&lt;start&gt; and /api/v1.0/&lt;start&gt;/&lt;end&gt; (&lt;start&gt; & &lt;end&gt; are dates in the format '%Y-%m-%d' (ex 2016-12-31)"
     )
 
 
@@ -35,18 +35,18 @@ def welcome():
 def precipitation():
     session = Session(engine)
 
-    results = session.query(Measurement.date, Measurement.prcp).all()
+    latest_date = session.query(Measurement.date).order_by(Measurement.date.desc()).first()._asdict()['date']
+    latest_date_dt = dt.datetime.strptime(latest_date, '%Y-%m-%d')
+    year_from_last = latest_date_dt - dt.timedelta(days=365)
+    results = session.query(Measurement.date, Measurement.prcp).filter(Measurement.date >= year_from_last).all()
 
     session.close()
 
-    all_measurements = []
+    measurement_dict = {}
     for date, prcp in results:
-        measurement_dict = {}
-        measurement_dict["date"] = date
-        measurement_dict["prcp"] = prcp
-        all_measurements.append(measurement_dict)
+        measurement_dict[date] = prcp
 
-    return jsonify(all_measurements)
+    return jsonify(measurement_dict)
 
 
 @app.route("/api/v1.0/stations")
@@ -66,23 +66,21 @@ def stations():
 def tobs():
     session = Session(engine)
 
-    # Query all passengers
-    latest_date = session.query(Measurement.date).order_by(Measurement.date.desc()).first()._asdict()['date']
+    most_active = session.query(Measurement.station, func.count(Measurement.station).label('total')).group_by(Measurement.station).order_by(text('total DESC')).first()[0]
+    latest_date = session.query(Measurement.date).filter(Measurement.station == most_active).order_by(Measurement.date.desc()).first()._asdict()['date']
     latest_date_dt = dt.datetime.strptime(latest_date, '%Y-%m-%d')
     year_from_last = latest_date_dt - dt.timedelta(days=365)
-    year_from_last_string = year_from_last.strftime("%Y-%m-%d")
-    year_from_last_string
-    most_active = session.query(Measurement.station, func.count(Measurement.station).label('total')).group_by(Measurement.station).order_by(text('total DESC')).first()[0]
-    data = session.query(Measurement.tobs).\
-      filter(Measurement.date >= year_from_last_string, Measurement.station == most_active).\
-      order_by(Measurement.date).all()
+    data = session.query(Measurement.tobs, Measurement.date).\
+      filter(Measurement.date >= year_from_last, Measurement.station == most_active).\
+      order_by(Measurement.date.desc()).all()
 
     session.close()
+    
+    measurement_dict = {}
+    for tobs, date in data:
+        measurement_dict[date] = tobs
 
-    # Create a dictionary from the row data and append to a list of all_passengers
-    last_year_tobs = list(np.ravel(data))
-
-    return jsonify(last_year_tobs)
+    return jsonify(measurement_dict)
 
 @app.route("/api/v1.0/<start>", defaults={"end": MAX_DATE})
 @app.route("/api/v1.0/<start>/<end>")
@@ -92,50 +90,22 @@ def dates(start, end):
       dt.datetime.strptime(start, "%Y-%m-%d")
       dt.datetime.strptime(end, "%Y-%m-%d")
     except ValueError:
-      # do your error message
-
+      return("Enter a valid date in format '%Y-%m-%d' (ex. 2016-12-31)")
     if start > end:
-      # another err
-    
-
-
-    # if start and not end:
-    #   try:
-    #     dt.datetime.strptime(start, "%Y-%m-%d")
-    #     session = Session(engine)
-    #     stats = session.query(func.min(Measurement.tobs), func.avg(Measurement.tobs), func.max(Measurement.tobs)).filter(Measurement.date >= start).first()
-    #     session.close()
-    #     stats_dict = {
-    #       'min_temp': stats[0],
-    #       'avg_temp': round(stats[1]),
-    #       'max_temp': stats[2]
-    #     }
-    #     return jsonify(stats_dict)
-    #   except ValueError as err:
-    #     return('start date not valid %Y-%m-%d (2010-12-31)')
- 
-    # if start and end:
-    #   try:
-        # start_dt = dt.datetime.strptime(start, "%Y-%m-%d")
-        # end_dt = dt.datetime.strptime(end, "%Y-%m-%d")
-        # if start_dt > end_dt:
-        #   return('start date must be before end date')
-        session = Session(engine)
-        stats = session.query(func.min(Measurement.tobs), func.avg(Measurement.tobs), func.max(Measurement.tobs)).filter(Measurement.date >= start, Measurement.date <=  end).first()
-        session.close()
-        print(stats)
-        if len(stats) == 0:
-          return('no results')
-        # else:
-        #   stats_dict = {
-        #     'min_temp': stats[0],
-        #     'avg_temp': round(stats[1]),
-        #     'max_temp': stats[2]
-        #   }
-        # return jsonify(stats_dict)
-      # except ValueError  as err:
-      #   print(err)
-      #   return('error')
+      return("Start date cant be after end date")
+    session = Session(engine)
+    stats = session.query(func.min(Measurement.tobs), func.avg(Measurement.tobs), func.max(Measurement.tobs)).filter(Measurement.date >= start, Measurement.date <=  end).first()
+    session.close()
+    print(f'stats {stats}')
+    if not stats[0]:
+      return('no results')
+    else:
+      stats_dict = {
+        'min_temp': stats[0],
+        'avg_temp': round(stats[1]),
+        'max_temp': stats[2]
+      }
+      return jsonify(stats_dict)
 
 
 if __name__ == '__main__':
